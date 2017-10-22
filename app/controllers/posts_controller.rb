b762c8c3319deb5da5a0bfafdebc92b9f7bac78f
TanyaKut <tanikvetil.k@gmail.com>
2017-10-22 14:13:36 +0300

    add gem pg

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,35 @@
 class PostsController < ApplicationController
+before_action :authenticate_user!
+before_action :set_post, only: [:edit, :destroy, :update]
   def edit
   end
 
-  def new
+  def update
   end
 
-  def _form
+  def new
+  	@post = Post.new
   end
+
+  def create
+  	@post = current_user.post.build(post_params)
+  	if @post.save
+  		redirect_to root_to
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+  	params.require(:post).premite(:user_id, :title, :body)
+  end
+
+  def set_post
+  	@post = Post.find(params [:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/edit'
-
-  get 'posts/new'
-
-  get 'posts/_form'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   root to: 'pages#index'
+
+  resource :post
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
