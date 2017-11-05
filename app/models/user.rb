47fe5182009c013acceb6f952cabc7cc24c6425b
TanyaKut <tanikvetil.k@gmail.com>
2017-11-05 22:34:03 +0200

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :postcomments
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         validates :password, confirmation: true
+         validates :nickname, length: { maximum: 10 }
+         validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+         validates :password, :email, :nickname, presence:true
 end
