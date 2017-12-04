b2772873827cdeb9e3daa056bf286350d59db385
TanyaKut <tanikvetil.k@gmail.com>
2017-12-04 21:00:12 +0200

    fix like

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,55 +1,55 @@
 class PhotosController < ApplicationController
-	before_action :authenticate_user!
+before_action :authenticate_user!
 
-  def index
-  	@photos = Photo.all
-  	@like = {}
-  	@photos.each do |photo|
-  		@vote = Vote.find_by_photo_id(photo.id)
-  	if !@vote.nil?
-  		@like[photo.id] = @vote.like
-  		end
-  	end
-  end
+def index
+@photos = Photo.all
+@like = {}
+@photos.each do |photo|
+@votes = Vote.where(photo_id: photo.id).sum(:like)
+@like[photo.id] = @votes
+end
 
-  def new
-  	@photo = Photo.new
-   end
+end
 
-   def create
-   	params[:photo][:category_id] = params[:category_id]
-   	params[:photo][:user_id] = current_user.id
-   	@photo = Photo.create(photo_params)
-   	redirect_to photos_path
-   end
+def new
+@photo = Photo.new
+end
 
-   def edit
-   end
+def create
+params[:photo][:category_id] = params[:category_id]
+params[:photo][:user_id] = current_user.id
+params[:photo][:photo]
+@photo = Photo.create(photo_params)
+redirect_to photos_path
+end
 
- 
+def edit
+end
 
-   def update
-   end
+def update
+end
 
-   def destroy
-   end
+def destroy
+@photo.destroy
+redirect_to photos_path
+end
 
-   def vote
-   	pp @photo = Photo.find(params[:id])
-   	pp @vote = Vote.where(user_id: current_user.id, photo_id: @photo.id).first
-
-   	if @vote == nil
-   		@like = 1
-   		Vote.create(user_id: current_user.id, photo_id: @photo.id, like: @like)
-   	else
-   		@vote.destroy;
-   	end
-   	redirect_to photos_path
-   end
+def vote
+@photo = Photo.find(params[:id])
+@vote = Vote.where(user_id: current_user.id, photo_id: @photo.id).first
+if @vote == nil or @vote.like == 0
+@like = 1
+Vote.create(user_id: current_user.id, photo_id: @photo.id, like: @like)
+else
+@vote.update(like: @vote.like-1)
+end
+redirect_to photos_path
+end
 
 private
 
-   def photo_params
-   	params.require(:photo).permit(:user_id, :category_id, :name, :photo)
-   end
+def photo_params
+params.require(:photo).permit(:user_id, :category_id, :name, :photo)
+end
+
 end
